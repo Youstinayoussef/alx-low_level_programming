fdff84893ec8f38b7940c86916543b721fb7e037
Yousa <me@example.com>
2023-09-10 13:53:47 -0700

    yous

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+    puts("\"programming is like building a multilingual puzzle");
+    return (0);
+}
+
